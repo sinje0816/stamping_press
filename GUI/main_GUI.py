f5f269cef705fcb78d23412531a96247f4a6f3b2
USER <C109104260@nkust.edu.tw>
2022-11-22 20:54:12 +0800

    UI

--- GUI/main_GUI.py
+++ GUI/main_GUI.py
@@ -91,23 +91,23 @@ class main(QtWidgets.QWidget, Ui_Dialog):
         self.ui.comboBox_3.clear()
         self.ui.comboBox_3.addItems(close_h)
     def start(self):
-        change = str(self.ui.comboBox_2.currentText())
+        l = str(self.ui.comboBox_2.currentText())
         height = str(self.ui.comboBox_3.currentText())
         type = str(self.ui.comboBox_4.currentText())
         hole = str(self.ui.comboBox_5.currentText())
-        print(type , change , height , hole)
+        print(type , l , height , hole)
         self.create_dir(type)
-        self.l , self.h ,self.i ,self.j = self.choos(change , height , type , hole)
+        self.l , self.h ,self.i ,self.j = self.choos(l , height , type , hole)
         self.change_dir( self.l , self.h ,self.i ,self.j , self.path)
         self.ass_(self.l , self.h ,self.i ,self.j , self.path)
 
 
 
 
-    def choos(self , change , height , type , hole):
+    def choos(self , l , height , type , hole):
         # 確認型號"輸入型號"
         # 確認型號"輸入型號"
-        type = input()
+        # type = input()
         if type == "SN1-25" or type == "sn1-25" or type == "25":
             i = 0
         elif type == "SN1-35" or type == "sn1-35" or type == "35":
@@ -131,7 +131,7 @@ class main(QtWidgets.QWidget, Ui_Dialog):
         print(i)
 
         # 合模高
-        height = input()
+        # height = input()
         if type == "SN1-25" and height == '230':
             h = 0
         elif type == "SN1-25" and height == '200':
@@ -175,7 +175,7 @@ class main(QtWidgets.QWidget, Ui_Dialog):
         print(h)
 
         # 判斷長寬
-        l = input()
+        # l = input()
         if l == '720x1058':
             l = 0
         elif l == '1080x1587':
@@ -249,7 +249,7 @@ class main(QtWidgets.QWidget, Ui_Dialog):
         os.mkdir(path)
         self.path = path
 
-    def change_dir(self, h , i , l , j , k , path):
+    def change_dir(self, h , i , l , j, path):
 
         # 開啟CATIA
         env = mprog.set_CATIA_workbench_env()
@@ -384,7 +384,7 @@ class main(QtWidgets.QWidget, Ui_Dialog):
                     else:
                         mprog.save_file_part(path, y)
 
-    def ass_(self, h , i , l , j , k , path):
+    def ass_(self, h , i , l , j , path):
 
         # 開啟新組合檔
         mprog.assembly_create()
