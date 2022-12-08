cb6ae55e33b0f4082600aa547956063512497d3a
USER <C109104260@nkust.edu.tw>
2022-12-08 22:17:40 +0800

    N_teat測試檔 N1 測試 drafting 2 test檔

--- GUI/drafting_test.py
+++ GUI/drafting_test.py
@@ -16,21 +16,22 @@ def drafting_parameter_calculation(width , height , depth , scale_p):
     h_scale = height*scale
     d_scale = depth*scale
 #--------------------------------左壁板虛擬方框範圍計算----------------------------
-    part1_range_center = [circle_code_1_center[0]+d_scale+20 , circle_code_1_center[1]-10-h_scale/2]#左壁板中心範圍[x , y]
+    part1_range_center = [circle_code_1_center[0]+d_scale+20+10 , circle_code_1_center[1]-10-h_scale/2]#左壁板中心範圍[x , y]
     part1_range = [part1_range_center[0]-d_scale-20-10-5 , part1_range_center[1]-h_scale/2-15 ,
-                   part1_range_center[0]+20+d_scale+10 , part1_range_center[1]+h_scale/2-15]#左壁板虛擬方框[Xmin , Ymin , Xmax , Ymax]
+                   part1_range_center[0]+20+d_scale+10 , part1_range_center[1]+h_scale/2+15]#左壁板虛擬方框[Xmin , Ymin , Xmax , Ymax]
     while True:
-        if part1_range [0] < 20:
+        if part1_range [0] < 20:#防止方框X方向超出圖框
             circle_code_1_center[0] += 5
             part1_range_center[0] = circle_code_1_center[0]+d_scale+20
             part1_range[0] = part1_range_center[0]-d_scale-20-10-5
-            part1_range[1] = part1_range_center[0]+20+d_scale+10
+            part1_range[1] = part1_range_center[0]+20+d_scale+10#利用位移完的結果重新計算方框X範圍
         else:
             break
     print(part1_range_center)
     print(part1_range)
 #--------------------------------右壁板虛擬方框計算-------------------------------
     circle_code_2_center = [circle_code_1_center[0] , part1_range[1]-5]
+    part2_range_center = [circle_code_2_center[0]+d_scale+20 , circle_code_2_center[1]-10-h_scale/2]#左壁板中心範圍[x , y]
 
 
 
