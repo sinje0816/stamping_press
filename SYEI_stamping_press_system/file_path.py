2dae04ce00143b2e46b8a8b6d799f52e011adb89
USER <C109104260@nkust.edu.tw>
2023-02-08 18:41:46 +0800

    新增檔案路徑py檔 爆炸圖新增1.5

---
+++ SYEI_stamping_press_system/file_path.py
@@ -0,0 +1,6 @@
+import os
+#檔案路徑
+system_root =os.path.dirname(os.path.realpath(__file__))
+part = "\\stamping_press"
+Exploded_Views = "\\Exploded_Views.CATDrawing"
+detail_drawing ="\\detail_drawing.CATDrawing"
