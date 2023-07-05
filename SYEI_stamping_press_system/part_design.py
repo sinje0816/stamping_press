0f7ebba314f14d3db38d3dac8b46b1c27b47d7b1
simbalin1660 <simba0912015663@gmail.com>
2023-07-05 15:39:24 +0800

    修改part_design中函式activatefeature()中，新增草圖激活

--- SYEI_stamping_press_system/part_design.py
+++ SYEI_stamping_press_system/part_design.py
@@ -13,12 +13,16 @@ def activatehfeatrue(feature):
     remove = shapes.Item(target)
     sub_body = remove.Body
     sub_shapes = sub_body.Shapes
-    shapes_cout = sub_shapes.Count
+    shapes_count = sub_shapes.Count
 
     # here to start activating process
     part.Activate(remove)
-    for i in range(1, shapes_cout + 1):
+    for i in range(1, shapes_count + 1):
         item_obj = sub_shapes.Item(i)
+        sketch = item_obj.Sketch
+        part.Activate(sketch)
         part.Activate(item_obj)
 
     part.Update()
+
+activatehfeatrue('3-M8通')
